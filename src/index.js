22a7fc5aac1cc42fb598e59cab3d1b7c0aa32efe
Ashwani Sahu <ashwani.s@honeysys.com>
2024-08-03 22:05:03 +0530

    basic setup of backend is done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,48 @@
 const express = require("express");
 const app = express();
 // const connect = require("./Configs/db")
-const Product=require("../src/Models/product.model")
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
-dotenv.config(); 
-mongoose.set('strictQuery', false)
-const PORT = process.env.PORT || 4000;
-app.use(cors())
-app.use(express.json())
-const start = async () => { 
-    try {
-        await mongoose.connect(`mongodb+srv://Ashwani:112345678901@productcluster.3wdobiq.mongodb.net/?retryWrites=true&w=majority`,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }, () => { 
-            console.log("connected to db")
-        });
-        app.listen(PORT, async () => { 
+// const Product=require("../src/Models/product.model")
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+// dotenv.config(); 
+// mongoose.set('strictQuery', false)
+// const PORT = process.env.PORT || 4000;
+// app.use(cors())
+// app.use(express.json())
+// const start = async () => { 
+//     try {
+//         await mongoose.connect(`mongodb+srv://Ashwani:112345678901@productcluster.3wdobiq.mongodb.net/?retryWrites=true&w=majority`,{
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         }, () => { 
+//             console.log("connected to db")
+//         });
+//         app.listen(PORT, async () => { 
             
-            console.log(`listening at port 4000`);
-        })
-    } catch (error) { 
-        console.log(error.message);
-    }
-}
-start();
-app.use("/", async (req,res) => { 
-    try {
-        const products = await Product.find({});
-        res.send({products})
-    } catch (error) { 
-        res.send({"message":"error is occuring"})
-    }
+//             console.log(`listening at port 4000`);
+//         })
+//     } catch (error) { 
+//         console.log(error.message);
+//     }
+// }
+// start();
+// app.use("/", async (req,res) => { 
+//     try {
+//         const products = await Product.find({});
+//         res.send({products})
+//     } catch (error) { 
+//         res.send({"message":"error is occuring"})
+//     }
+// })
+// console.log("app",app)
+
+const port = 5050;
+app.get('/',async(req,res)=>{
+    res?.send("Hello, world!,how are you all");
+})
+
+app.listen(port,()=>{
+    console.log(`listening on the port ${port}`);
 })
 
